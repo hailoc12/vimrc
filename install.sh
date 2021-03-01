ce923b390b45e8755dc5c46f0c0b48e1448a1e54
hailoc12 <hailoc12@gmail.com>
2021-03-01 15:46:07 +0000

    maintenance/improve readme

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 set -e
 export local_dir="vimrc"
 
-#init submodule
-# cd sources_non_forked/YouCompleteMe/
+# get all source codes
 git submodule update --init --recursive
 
 
-#delete old data and copy new one
+# replace current vim plugins with new plugins
 sudo rm -rf ~/.vim_runtime
 sudo cp -r ~/$local_dir ~/.vim_runtime
 
 cd ~/.vim_runtime
 
+# add vim_runtime to vim runtimepath (install all plugins in this folders)
 echo "set runtimepath+=~/.vim_runtime
 
 source ~/.vim_runtime/vimrcs/basic.vim
