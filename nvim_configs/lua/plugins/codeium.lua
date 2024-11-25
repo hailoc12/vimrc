d8a526890a1a5b995661a119b74d09bdd1bc28b4
Đoàn Văn Hiếu <doanhieu9797@gmail.com>
2024-11-25 16:57:58 +0000

    fix/codeium

--- nvim_configs/lua/plugins/codeium.lua
+++ nvim_configs/lua/plugins/codeium.lua
@@ -1,11 +1,5 @@
 return {
-    "Exafunction/codeium.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp",
-    },
-    config = function()
-        require("codeium").setup({
-        })
-    end
+    "Exafunction/codeium.vim",
+    event = 'BufEnter'
 }
+
